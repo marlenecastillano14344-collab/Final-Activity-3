53596f4dba49d371b6acb3e236f91d5825a92e94
marlenecastillano14344-collab <marlenecastillano14344@gmail.com>
2026-05-12 16:08:19 +0800

    Create lab17.php

---
+++ lab17.php
@@ -0,0 +1,15 @@
+<form method="POST">
+
+Name:
+<input type="text" required>
+
+<br><br>
+
+Email:
+<input type="email" value="marlenecastillano14344@gmail.com" required>
+
+<br><br>
+
+<input type="submit">
+
+</form>
