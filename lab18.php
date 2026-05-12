fb6fdd878a52a1feb5364919dc21cf97f0cb5edf
marlenecastillano14344-collab <marlenecastillano14344@gmail.com>
2026-05-12 16:09:05 +0800

    Create lab18.php

---
+++ lab18.php
@@ -0,0 +1,21 @@
+<h2>GET Method</h2>
+
+<form method="GET">
+
+Name:
+<input type="text" name="name">
+
+<input type="submit">
+
+</form>
+
+<h2>POST Method</h2>
+
+<form method="POST">
+
+Name:
+<input type="text" name="name">
+
+<input type="submit">
+
+</form>
