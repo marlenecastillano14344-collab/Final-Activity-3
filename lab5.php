a9b9e5ce8efa5d6324dd9215c4c337deff960157
marlenecastillano14344-collab <marlenecastillano14344@gmail.com>
2026-05-12 15:33:08 +0800

    Update lab5.php

--- lab5.php
+++ lab5.php
@@ -1,1 +1,18 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    $name = htmlspecialchars($_POST["name"]);
+
+    echo "Name: " . $name;
+}
+
+?>
+
+<form method="POST">
+
+Name:
+<input type="text" name="name" value="Marlene D. Castillano">
+
+<input type="submit">
+
+</form>
